53a39b5d0f69814b9f4b8bc606cee0cc900aa596
neekyo <codebyns@gmail.com>
2024-09-20 11:28:23 -0400

    add book & bookshow

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { useState } from 'react';
 import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
 
 function App() {
 	const [books, setBooks] = useState([]);
 
 	const createBook = (title) => {
-		console.log('need to add book with:', title);
+		const updateBooks = [
+			...books,
+			{ id: Math.round(Math.random() * 9999), title },
+		];
+		setBooks(updateBooks);
 	};
 
 	return (
-		<div>
+		<div className='app'>
+			<BookList books={books} />
 			<BookCreate onCreate={createBook} />
 		</div>
 	);
