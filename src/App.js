8d3caa3aa28ceb42a813e03c51060690bc3f1f95
neekyo <codebyns@gmail.com>
2024-09-23 10:57:35 -0400

    delete records & bookEdit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import BookList from './components/BookList';
 function App() {
 	const [books, setBooks] = useState([]);
 
+	const deleteBookById = (id) => {
+		const updatedBooks = books.filter((book) => {
+			return book.id !== id;
+		});
+
+		setBooks(updatedBooks);
+	};
 	const createBook = (title) => {
 		const updateBooks = [
 			...books,
@@ -15,7 +22,10 @@ function App() {
 
 	return (
 		<div className='app'>
-			<BookList books={books} />
+			<BookList
+				books={books}
+				onDelete={deleteBookById}
+			/>
 			<BookCreate onCreate={createBook} />
 		</div>
 	);
