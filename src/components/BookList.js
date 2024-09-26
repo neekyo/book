548a0cd76f554615b6f18a251617cf6f72784206
neekyo <codebyns@gmail.com>
2024-09-26 14:36:57 -0400

    db for data persistence & context/provider wired

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import BooksContext from '../context/books';
 import BookShow from './BookShow';
 
 function BookList({ books, onDelete, onEdit }) {
+	const { count, incrementCount } = useContext(BooksContext);
+
 	const renderedBooks = books.map((book) => {
 		return (
 			<BookShow
@@ -12,7 +16,13 @@ function BookList({ books, onDelete, onEdit }) {
 		);
 	});
 
-	return <div className='book-list'>{renderedBooks}</div>;
+	return (
+		<div className='book-list'>
+			{count}
+			<button onClick={incrementCount}>Click</button>
+			{renderedBooks}
+		</div>
+	);
 }
 
 export default BookList;
