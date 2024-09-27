1e1c8b454667bfc5b94936f984ca852ea61569a5
neekyo <codebyns@gmail.com>
2024-09-27 11:58:47 -0400

    context refactor pt 1

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -3,8 +3,6 @@ import BooksContext from '../context/books';
 import BookShow from './BookShow';
 
 function BookList({ books, onDelete, onEdit }) {
-	const { count, incrementCount } = useContext(BooksContext);
-
 	const renderedBooks = books.map((book) => {
 		return (
 			<BookShow
@@ -16,13 +14,7 @@ function BookList({ books, onDelete, onEdit }) {
 		);
 	});
 
-	return (
-		<div className='book-list'>
-			{count}
-			<button onClick={incrementCount}>Click</button>
-			{renderedBooks}
-		</div>
-	);
+	return <div className='book-list'>{renderedBooks}</div>;
 }
 
 export default BookList;
